810dfef2f6619101edc4f9876fcd303c08ee5ba5
Mathew Glass <hello@mathew.glass>
2025-05-09 21:33:02 -0400

    Added new page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Next.js on GitHub Pages',
-  description: 'Deploy your static Next.js site to GitHub Pages.',
+  title: 'Glass',
+  description: 'Static Page',
 };
 
 export default function RootLayout({
